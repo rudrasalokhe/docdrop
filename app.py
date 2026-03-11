2d11eb39061e70fd62d07124aaa6c7118309619b
rudrasalokhe <rudrasalokhe100@gmail.com>
2026-03-12 00:32:32 +0530

    updated some files

--- app.py
+++ app.py
@@ -574,4 +574,4 @@ Be professional, efficient, and clinically precise. Always maintain patient conf
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
